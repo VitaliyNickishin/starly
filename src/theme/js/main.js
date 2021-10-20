f90a3ca4d8727ff28c2e6548c15cead943b2af71
vitaliynickishin <nickishin34@gmail.com>
2021-10-21 01:31:23 +0300

    made header, mobile menu

--- src/theme/js/main.js
+++ src/theme/js/main.js
@@ -2,7 +2,7 @@ $(function() {
   window.appScroll = 0
 
   // Menu links
-  var links = $('header.main').find('li > a')
+  var links = $('.header').find('li > a')
   links.on('click', function() {
     let target = $(this).attr('href'),
         anchor = target.split('#')
@@ -24,77 +24,12 @@ $(function() {
     return false
   })
 
-  // Countdown timer
-  var countDownBox = $('#counter'),
-      countDownDate = null,
-      countDownItems = countDownBox.find('li > span > i'),
-      endDate = countDownBox.data('timer')
-  if(endDate) {
-    countDownDate = new Date(endDate).getTime()
-    var countInterval = setInterval(function() {
-      var now = new Date().getTime(),
-          distance = countDownDate - now,
-          days = Math.floor(distance / (1000 * 60 * 60 * 24)),
-          hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-          minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
-          seconds = Math.floor((distance % (1000 * 60)) / 1000)
-      days = String((days < 10) ? '0' + days : days)
-      hours = String((hours < 10) ? '0' + hours : hours)
-      minutes = String((minutes < 10) ? '0' + minutes : minutes)
-      seconds = String((seconds < 10) ? '0' + seconds : seconds)
-      if(typeof countDownItems[0] != 'undefined') {
-        $(countDownItems[0]).html(days[0])
-        $(countDownItems[1]).html(days[1])
-      }
-      if(typeof countDownItems[1] != 'undefined') {
-        $(countDownItems[2]).html(hours[0])
-        $(countDownItems[3]).html(hours[1])
-      }
-      if(typeof countDownItems[2] != 'undefined') {
-        $(countDownItems[4]).html(minutes[0])
-        $(countDownItems[5]).html(minutes[1])
-      }
-      if(typeof countDownItems[3] != 'undefined') {
-        $(countDownItems[6]).html(seconds[0])
-        $(countDownItems[7]).html(seconds[1])
-      }
-      if(distance < 0) {
-        clearInterval(countInterval)
-      }
-    }, 1000)
-  }
 
-  // Media slider
-  var swiper = new Swiper('.mySwiper', {
-    slidesPerView: 'auto',
-    breakpoints: {
-      320: {spaceBetween: 20},
-      480: {spaceBetween: 30},
-      576: {spaceBetween: 40},
-      768: {spaceBetween: 50},
-      920: {spaceBetween: 60},
-      1024: {spaceBetween: 74}
-    }
-  });
 
-  // LoadVideo
-  var videoBtn = $('.loadvideo'),
-      videoBox = videoBtn.parents('.box')
-  videoBtn.on('click', function() {
-    let el = $(this),
-        link = 'https://www.youtube.com/embed/' + el.data('id') + '?autoplay=1&autohide=1',
-        frame = $('<iframe/>', {
-          'frameborder': '0',
-          'src': link,
-          'width': videoBox.width(),
-          'height': videoBox.height()
-        })
-    el.replaceWith(frame)
-  })
-
+  
   // Mobile menu
-  var menuBtn = $('header.main').find('.mobilemenu'),
-      menuBox = $('header.main').find('ul'),
+  var menuBtn = $('.header').find('.burger'),
+      menuBox = $('.header').find('ul'),
       menuLink = menuBox.find('a')
   menuBtn.on('click', function() {
     menuBtn.toggleClass('active')
@@ -165,7 +100,7 @@ $(window).on('load', function() {
 })
 
 $(window).on('scroll', function () {
-  var header = $('header.main'),
+  var header = $('.header'),
       appScroll = $(this).scrollTop()
   if(appScroll > 0) {
     header.addClass('scrolled')
