5bf070cb785f21bfdee7cfc72a00778936203f2e
vitaliynickishin <nickishin34@gmail.com>
2021-11-01 05:06:55 +0200

    refactor js

--- src/theme/js/main.js
+++ src/theme/js/main.js
@@ -3,31 +3,6 @@ $(function() {
   headerFixed();
   initSliderRoadmap();
 
-  // Menu links
-  var links = $('.header').find('li > a')
-  links.on('click', function() {
-    let target = $(this).attr('href'),
-        anchor = target.split('#')
-    if(anchor.length == 2) {
-      if(target == '#media' || window.location.pathname == '/') {
-        let offset = Number($(target).css('padding-top').replace('px', ''))
-        offset += Number($(target).css('margin-top').replace('px', ''))
-        offset = (target == '#games') ? offset + 50 : offset - 100
-        $('html, body').animate({
-          scrollTop: $(target).position().top + offset
-        }, 1000)
-      } else {
-        localStorage.setItem('target', target)
-        window.location.replace('/')
-      }
-    } else {
-      window.location.replace(anchor)
-    }
-    return false
-  })
-
-
-
   
   // Mobile menu
   var menuBtn = $('.header').find('.burger'),
@@ -83,67 +58,10 @@ $(function() {
       }
     }, 1000)
   }
-
-  // Modal
-  var modalBtn = $('.openmodal'),
-      modalBox = $('.modalbox'),
-      modalSuccess = $('.modalsuccess')
-  modalBtn.on('click', function() {
-    if($(this).hasClass('active')) {
-      modalBox.removeClass('open')
-      setTimeout(function() {
-        modalBox.removeClass('active')
-      }, 350)
-    } else {
-      modalBox.addClass('active')
-      setTimeout(function() {
-        modalBox.addClass('open')
-      }, 350)
-    }
-    modalBtn.toggleClass('active')
-  })
-  modalBox.on('click', function(e) {
-    if(!$(e.target).closest('.modalinfo').length) {
-      modalBox.removeClass('open')
-      setTimeout(function() {
-        modalBox.removeClass('active')
-      }, 350)
-      modalBtn.toggleClass('active')
-    }
-  })
-  modalSuccess.on('click', function(e) {
-    if($(e.target).hasClass('close_modalsuccess') || !$(e.target).closest('.info').length) {
-      modalSuccess.removeClass('open')
-    }
-  })
-
-  // Accordion
-  var accordionBtn = $('button.accordionbtn')
-  accordionBtn.on('click', function() {
-    let el = $(this)
-    el.parents('.accordion').find('.info').slideToggle()
-    el.toggleClass('open')
-  })
-})
-
-$(window).on('load', function() {
-  if(window.location.pathname == '/' && localStorage.getItem('target')) {
-    let target = $(localStorage.getItem('target')),
-        offset = Number(target.css('padding-top').replace('px', ''))
-        offset += Number(target.css('margin-top').replace('px', ''))
-    offset = (localStorage.getItem('target') == '#games') ? offset + 50 : offset - 100
-    $('html, body').animate({
-      scrollTop: target.position().top + offset - 100
-    }, 1000, 'swing', function() {
-      localStorage.removeItem('target')
-    })
-  }
-  $('.modalbox, .modalsuccess').show(0)
-})
-
+});
 
 /* sticky header */ 
-function headerFixed() {
+let headerFixed = function () {
     let header = jQuery('.header');
     
 	$(window).on('scroll', function () {
@@ -156,7 +74,7 @@ function headerFixed() {
 	});
 }
 
-//sleder Featured in mobile
+//sleder Roadmap
 let initSliderRoadmap = function () {
   $('.roadmap-slider').slick({
     variableWidth: true,
@@ -165,8 +83,6 @@ let initSliderRoadmap = function () {
     arrows: false,
     speed: 300,
     centerMode: true,
-    // adaptiveHeight: true,
-    // centerPadding: '100px',
     infinite: false,
     responsive: [
       {
@@ -179,4 +95,5 @@ let initSliderRoadmap = function () {
       
       ]
     });
+    
 };
