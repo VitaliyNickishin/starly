f2b4f58f581891c253a8be0ed96f89f64e977f14
vitaliynickishin <nickishin34@gmail.com>
2021-11-08 00:57:50 +0200

    added animation for meteorite

--- src/theme/js/main.js
+++ src/theme/js/main.js
@@ -2,6 +2,8 @@ $(function() {
 
   headerFixed();
   initSliderRoadmap();
+  // initStarSky();
+  // initParticlesJS();
 
   
   // Mobile menu
@@ -99,12 +101,132 @@ let initSliderRoadmap = function () {
     });
     
     roadmapSlider.on('wheel', function(e) {
-        e.preventDefault();
-        if (e.originalEvent.deltaX > 0) {
-            $(this).slick('slickNext');
-        } else {
-            $(this).slick('slickPrev');
+      e.preventDefault();
+      if (e.originalEvent.deltaX > 0) {
+          $(this).slick('slickNext');
+      } else {
+          $(this).slick('slickPrev');
+      }
+  });
+};
+
+//background Star Sky
+let initStarSky = function () {
+  for (var i = 0; i < 100; i++) {
+    var star = '<div class="star-sky" style="animation: twinkle '+((Math.random()*5) + 5)+'s linear '+((Math.random()*5) + 5)+'s infinite; top: '+Math.random()*$(window).height()+'px; left: '+Math.random()*$(window).width()+'px;"></div>';
+    $('.hero, .benefit, .best-team, .roadmap').append(star);
+  }
+}
+
+let initParticlesJS = function () {
+  particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 355,
+        "density": {
+          "enable": true,
+          "value_area": 789.1476416322727
         }
-    });
-  };
-  
+      },
+      "color": {
+        "value": "#ffffff"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#000000"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+        "image": {
+          "src": "img/github.svg",
+          "width": 100,
+          "height": 100
+        }
+      },
+      "opacity": {
+        "value": 0.48927153781200905,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 0.2,
+          "opacity_min": 0,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 2,
+          "size_min": 0,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 0.2,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 400,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 83.91608391608392,
+          "size": 1,
+          "duration": 3,
+          "opacity": 1,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+}
